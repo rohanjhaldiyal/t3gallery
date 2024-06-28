6f2a04ccd3a2956b156ffb724a31b28a1650d535
Rohan Jhaldiyal <rohanjhaldiyal@gmail.com>
2024-06-28 23:16:40 +0530

    finalize modal and parallel image routes

--- src/app/@modal/(.)img/[id]/modal.tsx
+++ src/app/@modal/(.)img/[id]/modal.tsx
@@ -19,7 +19,7 @@ export function Modal({ children }: { children: React.ReactNode }) {
   }
 
   return createPortal(
-    <dialog ref={dialogRef} className="w-screen h-screen text-white bg-zinc-900/50 m-0" onClose={onDismiss}>
+    <dialog ref={dialogRef} className="w-screen h-screen text-white bg-black/90 m-0" onClose={onDismiss}>
       {children}
       {/* <button onClick={onDismiss} className="close-button" /> */}
     </dialog>,
