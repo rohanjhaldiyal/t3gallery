415bc2b98434303d34ff3315eb2d38bc8d42a794
Rohan Jhaldiyal <rohanjhaldiyal@gmail.com>
2024-06-28 22:54:29 +0530

    chore: Update modal component to display full-size images and additional information

--- src/app/@modal/(.)img/[id]/modal.tsx
+++ src/app/@modal/(.)img/[id]/modal.tsx
@@ -19,9 +19,9 @@ export function Modal({ children }: { children: React.ReactNode }) {
   }
 
   return createPortal(
-    <dialog ref={dialogRef} className="w-screen h-screen bg-zinc-900/50 m-0" onClose={onDismiss}>
+    <dialog ref={dialogRef} className="w-screen h-screen text-white bg-zinc-900/50 m-0" onClose={onDismiss}>
       {children}
-      <button onClick={onDismiss} className="close-button" />
+      {/* <button onClick={onDismiss} className="close-button" /> */}
     </dialog>,
     document.getElementById("modal-root")!,
   );
