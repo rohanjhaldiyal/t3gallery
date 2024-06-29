b1334236e23b11b79ab15cc0399f983355ce47bb
Rohan Jhaldiyal <rohanjhaldiyal@gmail.com>
2024-06-29 13:50:00 +0530

    Refactor import paths for FullPageImageView component

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { Modal } from "./modal";
-import FullPageImageView from "~/components/full-image-page";
+import FullPageImageView from "~/common/full-image-page";
 
 export default function PhotoModal({
   params: { id: photoId },
