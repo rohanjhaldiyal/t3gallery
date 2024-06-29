2f3a10c10db0222161e7624ec46d952759ee470f
Rohan Jhaldiyal <rohanjhaldiyal@gmail.com>
2024-06-29 14:27:49 +0530

    delete button

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -1,6 +1,9 @@
 import { auth } from "@clerk/nextjs/server";
 import "server-only";
 import { db } from "~/server/db";
+import { images } from "./db/schema";
+import { and, eq } from "drizzle-orm";
+import { redirect } from "next/navigation";
 
 export async function getMyImages() {
   const user = auth();
@@ -24,4 +27,14 @@ export async function getImage(id: number) {
   if (!image) throw new Error("Image Not found");
   if (image.userId !== user.userId) throw new Error("Unauthorized");
   return image;
+}
+
+export async function deleteImage(id: number) {
+  const user = auth();
+  if (!user.userId) throw new Error("Unauthorized");
+
+  await db
+    .delete(images)
+    .where(and(eq(images.id, id), eq(images.userId, user.userId)));
+  redirect("/");
 }
