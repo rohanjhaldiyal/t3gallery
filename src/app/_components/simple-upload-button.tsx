cf1f3932bd4ab9bfa3c4ba945ef77c214beeb2c6
Rohan Jhaldiyal <rohanjhaldiyal@gmail.com>
2024-06-29 13:21:32 +0530

    Add posthog analytics and capture upload_begin event

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation";
 import { useUploadThing } from "~/utils/uploadthing";
 import { toast } from "sonner";
+import { usePostHog } from "posthog-js/react";
 
 // inferred input off useUploadThing
 type Input = Parameters<typeof useUploadThing>;
@@ -72,8 +73,10 @@ function LoadingSpinnerSVG() {
 
 export function SimpleUploadButton() {
   const router = useRouter();
+  const posthog = usePostHog();
   const { inputProps } = useUploadThingInputProps("imageUploader", {
     onUploadBegin() {
+      posthog.capture("upload_begin");
       toast(
         <div className="flex gap-2 text-white items-center">
           <LoadingSpinnerSVG />
